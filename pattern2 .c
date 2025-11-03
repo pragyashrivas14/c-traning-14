03aa06f97a6516e5e6f5f1f93b8b3a4253497e37
pragyashrivas <pragyasrivas3@gmail.com>
2025-11-03 12:49:56 +0530

    pattern

--- pattern2 .c
+++ pattern2 .c
@@ -4,12 +4,16 @@ int main(){
     printf("Enter n:");
     scanf("%d",&n);
     for(int i=0;i<n;i++){
-        for (int j=n;j>=i;j--)
         printf("*");
+    
+    {
+        for (int j =0;j<n;j++)
+        printf(" ");
+    
     {
         printf("\n");
     }
-
+}
     }
 
 }
